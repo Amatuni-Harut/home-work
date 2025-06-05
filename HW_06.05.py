cc621d00b86d938495713aa75d431d254e89e6ee
harut <harutyun.amatuni.2@gmail.com>
2025-06-05 22:48:33 +0400

    home work 06.06

---
+++ HW_06.05.py
@@ -0,0 +1,22 @@
+import os
+def open_folder_reed_file(folder):
+    data=[]
+    if not os.path.exists(folder):
+        print("not folder")
+        return data
+    for fname in os.listdir(folder):
+        md=os.path.join(folder,fname)
+        with open(md, "r") as f:
+            file_line=f.readlines()
+            data.extend([el.strip( ) for el in file_line if el.strip()])
+    return data            
+
+def write_info_file(md, fname):
+    with open (fname, "w") as f:
+        for el in md:
+            f.write(el + '\n')
+def main():
+    folder="/home/harutyun/Desktop/home-work/dir1"
+    prog_logic=open_folder_reed_file(folder)
+    write_info_file(prog_logic,"result_06.05.txt")
+main()
