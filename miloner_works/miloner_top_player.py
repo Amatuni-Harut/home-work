c3893f113340314e062e92665f3b6fcd8265d333
harut <harutyun.amatuni.2@gmail.com>
2025-07-11 19:01:33 +0400

    new

--- miloner_works/miloner_top_player.py
+++
@@ -1,8 +0,0 @@
-class TopPlayer:
-    def __init__(self, filename):
-        self.__filename = filename
-
-    def saving_score_in_file(self, username, score):
-        with open(self.__filename, "a") as f:
-            f.write(f"{username}:{score}/10\n")
-        print("Score saved")
